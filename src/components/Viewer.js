b9f107374541df410a310d31b09f1cd2e19d5573
SirlLizz <sleepvalera@gmail.com>
2024-08-17 19:30:39 +0400

    All Done LittlePlanet

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -1,60 +1,49 @@
-import React, {useState} from 'react';
-import { ReactPhotoSphereViewer } from "react-photo-sphere-viewer";
+import React, {useState, useEffect} from 'react';
+import { Viewer } from "@photo-sphere-viewer/core";
 import UploadFile from './UploadFile';
 import Form from 'react-bootstrap/Form';
+import "@photo-sphere-viewer/core/index.css";
 
-class ViewerWindow extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            file: props.file ?? undefined,
-            isURL: typeof props.file == 'string',
-            isLittlePlanet: false
-        }
-    }
+const ViewerWindow = ({ url = undefined }) => {
+    const [isLittlePlanet, setIsLittlePlanet] = useState(false); 
+    const [isURL] = useState(typeof url == 'string'); 
+    const [file, setFile] = useState(url); 
+    const sphereElementRef = React.createRef();
 
-    fileHandler = (newFile) => {
-        this.setState({
-            file: newFile
-        })
-    }
-
-    isLittlePlanetHandler = (newIsLittlePlanet) => {
-        this.setState({
-            isLittlePlanet: newIsLittlePlanet
-        })
-    }
+    useEffect(() => {
+        if(file){
+            const shperePlayerInstance = new Viewer({
+                container: sphereElementRef.current,
+                panorama: isURL ? file : URL.createObjectURL(file),
+                size: { height: "100%", width: "100%" },
+                fisheye: isLittlePlanet ? 2 : 0,
+                maxFov: isLittlePlanet ? 150 : 100
+            });
         
-    componentDidUpdate(){
-        this.render();
-    }
+            return () => {
+            shperePlayerInstance.destroy();
+            };
+        }
 
-    render() {
-        const {file, isURL, isLittlePlanet} = this.state;
-        return (
-            <div className="flex-container flex-column">
-                <div className="mx-auto my-3 d-flex align-content-center">
-                    {!isURL &&(<UploadFile fileHandler = {this.fileHandler}/>)}
-                    <Form>
-                        <Form.Check
-                            type="switch"
-                            id="custom-switch"
-                            label="Little Planet"
-                            onChange={()=>{this.isLittlePlanetHandler(!isLittlePlanet)}}
-                        />
-                    </Form>
-                </div>
-                <div className="flex-stretch ">
-                    {file && (<ReactPhotoSphereViewer
-                        src={isURL ? file : URL.createObjectURL(file)}
-                        littlePlanet={isLittlePlanet}
-                        height={"100%"}
-                        width={"100%"}
-                    ></ReactPhotoSphereViewer>
-                    )}
-                </div>
+      }, [file, isLittlePlanet]); 
+
+    return (
+        <div className="flex-container flex-column">
+            <div className="mx-auto my-3 d-flex align-items-center">
+                {!isURL &&(<UploadFile fileHandler = {setFile}/>)}
+                <Form>
+                    <Form.Check
+                        type="switch"
+                        id="custom-switch"
+                        label="Little Planet"
+                        onChange={()=>{setIsLittlePlanet(!isLittlePlanet)}}
+                    />
+                </Form>
             </div>
-        );
-    }
+            <div className="flex-stretch ">
+                {file && (<div ref={sphereElementRef} style={{ height: "100%", width: "100%"}}/>)}
+            </div>
+        </div>
+    );
 }
 export default ViewerWindow;
